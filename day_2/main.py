ac7f909920bd6243f64464281e989b2140a06987
Pedro Avalos <pavalos6401@gmail.com>
2019-12-02 01:03:52 -0600

    setup for day 2

---
+++ day_2/main.py
@@ -0,0 +1,8 @@
+# Class for Day 2
+class Day2():
+    def part_1(self):
+        with open('day_2/data/intcodes.txt') as file:
+            lines = file.readlines()
+        intcodes = lines.split()
+        return 0
+
