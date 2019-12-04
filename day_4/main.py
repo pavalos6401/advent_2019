6e8aae40961ccb170bc91627ab3fcba7e206de71
Pedro Avalos <pavalos6401@gmail.com>
2019-12-04 10:02:19 -0600

    day 4 part 1 finished

--- day_4/main.py
+++ day_4/main.py
@@ -1,46 +1,46 @@
+# Directory of the data file
 FILE_DIR = 'day_4/data/range.txt'
 
+# Minimum and Maximum
 GIVEN_RANGE = list(map(int, open(FILE_DIR).readline().split('-')))
 
 class PasswordCracker():
     def __init__(self):
-        # Size requirement of password
+        # Min and Max constraints
+        self.min = GIVEN_RANGE[0]
+        self.max = GIVEN_RANGE[1]
+        # Size constraint of password
         self.size = 6
-        # Range of min to max
-        self.constraint = range(GIVEN_RANGE[0], GIVEN_RANGE[1])
-        print(GIVEN_RANGE)
         # Whether or not the 'twin' requirement is met
         self.twin = False
-        # Whether or not the password is ascending
+        # Whether or not the ascending requirement is met
         self.ascending = False
-        # List of possible passwords
+        # List of possible passwords (none when starting)
         self.passwords = []
-        # Number of passwords
-        self.num_of_passwords = 0
 
     def find_passwords(self):
-        for guess in self.constraint:
-            # Reset requirements (easiest to check for twin and check for NOT ascending)
+        for guess in range(self.min, self.max):
+            # Reset requirements
             self.twin = False
-            self.ascending = True
+            self.ascending = False
 
             # List of digits of the current guess
-            guess_digits = list(map(int, str(guess).split()))
+            guess_digits = [int(digit) for digit in str(guess)]
             
             # Check if there is at least one pair of twins
             for i in range(len(guess_digits) - 1):
                 if (guess_digits[i] == guess_digits[i + 1]):
-                    print("Twin")
                     self.twin = True
-            # Check if the 
-            for i in range(len(guess_digits) - 1):
-                if (guess_digits[i] > guess_digits[i + 1]):
-                    print("Ascending")
-                    self.ascending = False
+            # Check for the ascending requirement
+            if sorted(guess_digits) == guess_digits:
+                self.ascending = True
+            # Save the guess in the paswords
             if self.twin and self.ascending:
                 self.passwords.append(guess)
-                print("Yes")
 
+# Instance of the password cracker
 password_cracker = PasswordCracker()
+# Find all the possible passwords
 password_cracker.find_passwords()
-print(password_cracker.passwords)
+# Get the length of the list of possible passwords
+print(len(password_cracker.passwords))
