fb0f830888bb614505021a5f9b21b20a6e16e735
Pedro Avalos <pavalos6401@gmail.com>
2019-12-04 11:21:47 -0600

    progress on day 4 part 2

--- day_4/main.py
+++ day_4/main.py
@@ -15,9 +15,12 @@ class PasswordCracker():
         self.twin = False
         # Whether or not the ascending requirement is met
         self.ascending = False
-        # List of possible passwords (none when starting)
+        # List of possible passwords (none when starting) - part 1
         self.passwords = []
+        # List of real passwords (none when starting) - part 2
+        self.real_passwords = []
 
+    # Part 1
     def find_passwords(self):
         for guess in range(self.min, self.max):
             # Reset requirements
@@ -38,9 +41,37 @@ class PasswordCracker():
             if self.twin and self.ascending:
                 self.passwords.append(guess)
 
+    # Part 2
+    def find_real_passwords(self):
+        for password in self.passwords:
+            # Only requirement to check now is the new real twin requirement
+            self.twin = False
+
+            # List of digits of the current password
+            password_digits = [int(digit) for digit in str(password)]
+
+            # List of all the pairs
+            repeats_list = []
+
+            # Find all the pairs of twins
+            for i in range(len(password_digits) - 1):
+                if (password_digits[i] == password_digits[i + 1]):
+                    repeats_list.append(password_digits[i])
+
+            # Sort them highest to lowest
+            repeats_list.sort(reverse=True)
+
+            # If the highest twin is unique, then save the password
+            if repeats_list.count(repeats_list[0]) == 1:
+                self.real_passwords.append(password)
+                    
+
 # Instance of the password cracker
 password_cracker = PasswordCracker()
 # Find all the possible passwords
 password_cracker.find_passwords()
 # Get the length of the list of possible passwords
-print(len(password_cracker.passwords))
+print(f"Possible Passwords for Part 1: {len(password_cracker.passwords)}")
+password_cracker.find_real_passwords()
+# Get the lengths of the list of real passwords
+print(f"Possible Real Passwords for Part 2: {len(password_cracker.real_passwords)}")
